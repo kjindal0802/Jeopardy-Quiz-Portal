cb97ac23796aca3a3a954a063a2debdaa84797eb
kjindal0802 <kjindal0802@gmail.com>
2017-03-26 17:29:44 +0530

    Add files via upload

---
+++ quiz/new.php
@@ -0,0 +1,120 @@
+    <head>
+    	  <meta charset="utf-8">
+		<title>JEOPARDY HOME</title>		
+		<!-- Meta Description -->
+     
+		
+		<!-- CSS
+		================================================== -->
+		
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+		
+		<!-- Fontawesome Icon font -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/slit-slider.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/animate.css">
+		<!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/main.css">
+		<!-- Media Queries -->
+        <link rel="stylesheet" href="css/media-queries.css">
+		<!-- Modernizer Script for old Browsers -->
+        <script src="js/modernizr-2.6.2.min.js"></script>
+
+			<meta charset="utf-8">
+		  <link rel="stylesheet" href="css/font-awesome.min.css">
+		  <link rel="stylesheet" href="css/animate.css">
+		  <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900|Montserrat:400,700' rel='stylesheet' type='text/css'>
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+
+		  <link rel="stylesheet" href="css/bootstrap.min.css">
+		  <link rel="stylesheet" href="css/main.css">
+		  <link rel="stylesheet" href="css/font-awesome.min.css">
+		  <link rel="stylesheet" href="css/animate.css">
+		 
+
+		  <link rel="stylesheet" href="css/bootstrap.min.css">
+		  <link rel="stylesheet" href="css/main.css">
+
+	
+  
+</head>
+		<script src="js/modernizr-2.7.1.js"></script>
+  
+	
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				
+				<script src="js/bootstrap.min.js"></script>
+				
+			   <script src="js/wow.min.js"></script>
+						  <script>
+						  new WOW().init();
+						  </script>
+						  
+						  <style>
+						  #invite {
+    background: rgba(0, 0, 0, 0.68);
+}</style>
+<body style="background-image:url('145.jpg');height:500">
+
+<img src="147.png" class="wow fadeInUp" data-wow-duration="5s" style="margin-left:460;">
+<br>
+	  
+	  
+	 <section id="invite" class="pad-lg light-gray-bg" style="margin-top:-160;height:680">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2 text-center" style="margin-top:40;">
+         <i class="fa fa-question-circle" style="font-size:36px;color:white"></i>
+            <h2 class="black" style="color:white;">MURIOUS-XI</h2>
+            <br />
+            <p class="black" style="color:white;">Number of Teams.</p>
+            <br />
+			
+            <form action="quiz.php" method="post">
+	
+
+	
+	<select name="teams" class="form-control" >
+		<option value="1">1 team</option>		
+		<option value="2">2 teams</option>
+		<option value="3">3 teams</option>
+		<option value="4">4 teams</option>
+		<option value="5">5 teams</option>
+		<option value="6">6 teams</option>
+		<option value="7">7 teams</option>
+		<option value="8">8 teams</option>
+		<option value="9">9 teams</option>
+		<option value="10">10 teams</option>	
+		<option value="11">11 teams</option>
+		<option value="12">12 teams</option>	
+	</select>
+<br>
+	<input class="btn btn-primary btn-lg" type="submit" id="submit" value="Start" />
+
+	</form>
+			<Br><Br>
+        <p align="center">Press F11 for full-screen mode</p>  
+		<p>Click the following button, you will be redirected to home page.</p>
+         
+        </div>
+      </div>
+    </section>
+	
+	
+	</body>
+
+
+
+      
+   
+      
+
+
